20e4f3c7f9e2c7393c30eb8eafbfb0b1f850d9d0
a <zangedsu@gmail.com>
2024-08-05 13:04:20 +0300

    added attachment model

---
+++ app/Models/Attachment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Attachment extends Model
+{
+    protected $fillable = [
+        'path',
+        'type',
+        'name',
+        'team_id',
+        'user_id',
+    ];
+
+    public function team(): BelongsTo
+    {
+        return $this->belongsTo(Team::class);
+    }
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
+}
