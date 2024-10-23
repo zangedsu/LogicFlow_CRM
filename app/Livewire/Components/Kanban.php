1e9ac7e7e3f42e926b9617d7e88afd246b4456cf
a <zangedsu@gmail.com>
2024-10-23 12:26:00 +0300

    upd a kanban component

--- app/Livewire/Components/Kanban.php
+++ app/Livewire/Components/Kanban.php
@@ -11,36 +11,30 @@ class Kanban extends Component
 
     public $tasks;
 
-    public function updateTaskStatus($sorted) : void
+    public function updateTaskStatus($sorted): void
     {
         $states = ['new', 'in_process', 'completed', 'failed'];
-      foreach ($sorted as $item)
-      {
-          foreach ($item['items'] as $id){
-              $task = Auth::user()->currentTeam()->first()->tasks()->find($id['value']);
-              if($task?->state != $item['value'] && in_array($item['value'], $states)){
-                  $task->state = $item['value'];
-                  $task->save();
-              }
-          }
-      }
-      $this->updateTasks();
+        foreach ($sorted as $item) {
+            foreach ($item['items'] as $id) {
+                $task = Auth::user()->currentTeam()->first()->tasks()->find($id['value']);
+                if ($task?->state != $item['value'] && in_array($item['value'], $states)) {
+                    $task->state = $item['value'];
+                    $task->save();
+                }
+            }
+        }
+        $this->updateTasks();
     }
 
 
-    public function updateTasks()
+    public function updateTasks() : void
     {
         $this->tasks = Auth::user()->currentTeam()->first()->tasks()->get();
     }
+
     public function render()
     {
-    $this->updateTasks();
+        $this->updateTasks();
         return view('livewire.components.kanban');
-//        return view('livewire.components.kanban',[
-//            'newTasks' => Task::where('state', 'new')->get(),
-//            'inProgressTasks' => Task::where('state', 'in_process')->get(),
-//            'completedTasks' => Task::where('state', 'completed')->get(),
-//            'failedTasks' => Task::where('state', 'failed')->get(),
-//        ]);
     }
 }
